c376e29ec9f68ec8796e9dd3149054b5024c995c
innatfya <innalutfiah@gmail.com>
2025-12-07 00:48:09 +0700

    js profile

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", function () {
         ride: 'carousel',
         wrap: true      });
 });
+
+//profil dah jir
+const inputs = document.querySelectorAll("#name, #phone, #email, #bio");
+inputs.forEach(input => {
+    input.addEventListener("input", () => {
+        const counter = input.parentElement.querySelector(".char-counter");
+        counter.textContent = `${input.value.length}/${input.maxLength}`;
+        validateForm();});
+});
+function validateForm() {
+    const name = document.getElementById("name").value.trim();
+    const saveBtn = document.getElementById("saveBtn");
+    saveBtn.disabled = name.length === 0;}
