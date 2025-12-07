82d51b43da61805afaee902e4af234512d9acdf2
innatfya <innalutfiah@gmail.com>
2025-12-07 15:52:26 +0700

    benerin header

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const myCarousel = document.querySelector('#carouselExampleAutoplaying');
     const carousel = new bootstrap.Carousel(myCarousel, {
-        interval: 1000, 
+        interval: 3000, 
         ride: 'carousel',
         wrap: true      });
 });
